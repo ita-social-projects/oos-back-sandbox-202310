9924ca1d6261525381ef05824e93089894ea627c
Taranko Taras <tarastarnko2@gmail.com>
2023-10-25 20:42:04 +0300

    Aded achievement model

---
+++ OutOfSchool/OutOfSchool.DataAccess/Models/ChildrenAchievement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutOfSchool.Services.Models;
+public class ChildrenAchievement : IKeyedEntity<Guid>
+{
+    public Guid Id { get; set; }
+
+    public string Type { get; set; }
+
+    public DateTime Date { get; set; }
+
+    public string Trainer { get; set; }
+
+    public Guid ChildId { get; set; }
+
+    public Child Child { get; set; }
+
+    public Guid WorkshopId { get; set; }
+
+    public Workshop Workshop { get; set; }
+
+}
