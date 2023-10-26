a001536944e7c821264a49f4eca786b1da0eea28
Taranko Taras <tarastarnko2@gmail.com>
2023-10-26 20:00:25 +0300

    Aded achievement dto and methods for service

---
+++ OutOfSchool/OutOfSchool.WebApi/Models/ChildAchievementDto.cs
@@ -0,0 +1,17 @@
+﻿namespace OutOfSchool.WebApi.Models;
+
+public class ChildAchievementDto
+{
+    public Guid Id { get; set; }
+
+    public string Type { get; set; }
+
+    public DateTime Date { get; set; }
+
+    public string Trainer { get; set; }
+
+    public Guid ChildId { get; set; }
+
+    public Guid WorkshopId { get; set; }
+
+}
--- OutOfSchool/OutOfSchool.WebApi/Services/IChildAchievementService.cs
+++ OutOfSchool/OutOfSchool.WebApi/Services/IChildAchievementService.cs
@@ -1,5 +1,18 @@
-﻿namespace OutOfSchool.WebApi.Services;
+﻿using OutOfSchool.WebApi.Models;
+
+namespace OutOfSchool.WebApi.Services;
 
 public interface IChildAchievementService
 {
+    Task<ChildAchievementDto> CreateAchievement(ChildAchievementDto childAchievementDto);
+
+    public Task DeleteAchievement(ChildAchievementDto childAchievementDto);
+
+    public Task<ChildAchievementDto> UpdateAchievement(ChildAchievementDto childAchievementDto);
+
+    public Task<IEnumerable<ChildAchievementDto>> GetAchievementForChildId(Guid id);
+
+    public Task<IEnumerable<ChildAchievementDto>> GetAchievementForWorkshopId(Guid id);
+
+    public Task<IEnumerable<ChildAchievementDto>> GetAchievementForWorkshopIdChildId(Guid childId, Guid workshopId);
 }
