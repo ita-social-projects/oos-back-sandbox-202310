403e79b455150cab0f3852468074d7ae5788bac4
Taranko Taras <tarastarnko2@gmail.com>
2023-10-27 10:26:01 +0300

    Aded documentation for swagger to endpoints

--- OutOfSchool/OutOfSchool.WebApi/Controllers/V1/ChildAchievementController.cs
+++ OutOfSchool/OutOfSchool.WebApi/Controllers/V1/ChildAchievementController.cs
@@ -14,19 +14,54 @@ public class ChildAchievementController : ControllerBase
         this.service = service;
     }
 
+    /// <summary>
+    /// Method for creating a new child achievement.
+    /// </summary>
+    /// <param name="childAchievementCreationDto">Child achievement entity to add.</param>
+    /// <returns>The child achievement that was created.</returns>
+    //[HasPermission(Permissions.ChildAddNew)]
+    [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(ChildDto))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     [HttpPost]
     public async Task<IActionResult> Create(ChildAchievementCreationDto childAchievementCreationDto) {
         var newAchive = await service.CreateAchievement(childAchievementCreationDto);
-        return Ok(newAchive);
+        return CreatedAtAction(
+            nameof(newAchive),
+            newAchive);
     }
 
+    /// <summary>
+    /// Delete the child achievement from the database.
+    /// </summary>
+    /// <param name="id">The child achievement id.</param>
+    /// <returns>If deletion was successful, the result will be Status Code 204.</returns>
+    [HasPermission(Permissions.ChildRemove)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     [HttpDelete]
     public async Task<IActionResult> Delete(Guid id)
     {
         await service.DeleteAchievement(id);
-        return Ok();
+        return NoContent();
     }
 
+    /// <summary>
+    /// Update info about child achievement in the database.
+    /// </summary>
+    /// <param name="childAchievementDto">Child achievement entity to update.</param>
+    /// <returns>The child achievement that was updated.</returns>
+    [HasPermission(Permissions.ChildEdit)]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ChildDto))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     [HttpPut]
     public async Task<IActionResult> Update(ChildAchievementDto childAchievementDto)
     {
@@ -34,6 +69,17 @@ public class ChildAchievementController : ControllerBase
         return Ok(updatedAchive);
     }
 
+    /// <summary>
+    /// Get children achievements by child id from the database.
+    /// </summary>
+    /// <param name="id">Child id to get achievement entity.</param>
+    /// <returns>The child achievement that was founded.</returns>
+    [HasPermission(Permissions.SystemManagement)]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(SearchResult<ChildDto>))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     [HttpGet("GetForChildId")]
     public async Task<IActionResult> GetForChildId(Guid id)
     {
@@ -41,6 +87,17 @@ public class ChildAchievementController : ControllerBase
         return Ok(childAchievements);
     }
 
+    /// <summary>
+    /// Get children achievements by workshop id from the database.
+    /// </summary>
+    /// <param name="id">Workshop id to get achievement entity.</param>
+    /// <returns>The child achievement that was founded.</returns>
+    [HasPermission(Permissions.SystemManagement)]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(SearchResult<ChildDto>))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     [HttpGet("GetForWorkshopId")]
     public async Task<IActionResult> GetForWorkshopId(Guid id)
     {
@@ -48,6 +105,18 @@ public class ChildAchievementController : ControllerBase
         return Ok(childAchievements);
     }
 
+    /// <summary>
+    /// Get children achievements by child and workshop id's from the database.
+    /// </summary>
+    /// <param name="childId">Child id to get achievement entity.</param>
+    /// <param name="workshopId">Workshop id to get achievement entity.</param>
+    /// <returns>The child achievement that was founded.</returns>
+    [HasPermission(Permissions.SystemManagement)]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(SearchResult<ChildDto>))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     [HttpGet("GetForChildIdWorkshopId")]
     public async Task<IActionResult> GetForChildIdWorkshopId(Guid childId,Guid workshopId)
     {
