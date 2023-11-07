06f2fb8a60a9f04dfa341f6f977d7d9e266a64e5
Taranko Taras <tarastarnko2@gmail.com>
2023-11-07 18:18:28 +0200

    Aded swagger documentation for ministry admin controller

--- OutOfSchool/OutOfSchool.WebApi/Controllers/V1/MinistryAdminController.cs
+++ OutOfSchool/OutOfSchool.WebApi/Controllers/V1/MinistryAdminController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using OutOfSchool.WebApi.Common;
+using OutOfSchool.WebApi.Models;
 using OutOfSchool.WebApi.Models.ChildAchievement;
 using OutOfSchool.WebApi.Models.Ministry;
 
@@ -18,6 +19,16 @@ public class MinistryAdminController : ControllerBase
         this.service = service ?? throw new ArgumentNullException(nameof(service));
     }
 
+    /// <summary>
+    /// Method for creating a new ministery admin.
+    /// </summary>
+    /// <param name="ministryAdminCreationRequestDto">Ministery admin entity to add.</param>
+    /// <returns>The ministery admin that was created.</returns>
+    [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(MinistryAdminCreationResponseDto))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     [HttpPost]
     public async Task<IActionResult> Create(MinistryAdminCreationRequestDto ministryAdminCreationRequestDto)
     {
@@ -32,6 +43,16 @@ public class MinistryAdminController : ControllerBase
             newMinistryAdmin);
     }
 
+    /// <summary>
+    /// Delete the cministery admin from the database.
+    /// </summary>
+    /// <param name="id">The ministery admin id.</param>
+    /// <returns>If deletion was successful, the result will be Status Code 204.</returns>
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     [HttpDelete]
     public async Task<IActionResult> Delete(Guid id)
     {
@@ -44,6 +65,16 @@ public class MinistryAdminController : ControllerBase
         return NoContent();
     }
 
+    /// <summary>
+    /// Update info about ministery admin in the database.
+    /// </summary>
+    /// <param name="ministryAdminUpdatingDto">Ministery admin entity to update.</param>
+    /// <returns>The ministery admin that was updated.</returns>
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(MinistryAdminUpdatingDto))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     [HttpPut]
     public async Task<IActionResult> Update(MinistryAdminUpdatingDto ministryAdminUpdatingDto)
     {
@@ -56,6 +87,16 @@ public class MinistryAdminController : ControllerBase
         return Ok(updatedMinistryAdmin);
     }
 
+    /// <summary>
+    /// Approve iministery admin status in the database.
+    /// </summary>
+    /// <param name="id">Ministery admin id.</param>
+    /// <returns>If approve was successful, the result will be Status Code 200.</returns>
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     [HttpPut("Approve")]
     public async Task<IActionResult> Approve(Guid id)
     {
@@ -68,6 +109,16 @@ public class MinistryAdminController : ControllerBase
         return Ok(result);
     }
 
+    /// <summary>
+    /// Get all ministery admins.
+    /// </summary>
+    /// <returns>The ministery admins that was founded.</returns>
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(SearchResult<MinistryAdminGettingDto>))]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     [HttpGet("GetAll")]
     public async Task<IActionResult> GetAll()
     {
@@ -85,6 +136,16 @@ public class MinistryAdminController : ControllerBase
         return Ok(ministryAdmins);
     }
 
+    /// <summary>
+    /// Get ministery admins by ministery id from the database.
+    /// </summary>
+    /// <param name="id">Ministery id to get ministery admins entities.</param>
+    /// <returns>The ministery admins that was founded.</returns>
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(SearchResult<MinistryAdminGettingDto>))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     [HttpGet("GetForMinistrypId")]
     public async Task<IActionResult> GetForMinistrypId(int id)
     {
@@ -102,6 +163,16 @@ public class MinistryAdminController : ControllerBase
         return Ok(ministryAdmins);
     }
 
+    /// <summary>
+    /// Get ministery admins by id from the database.
+    /// </summary>
+    /// <param name="id">Id to get ministery admins entities.</param>
+    /// <returns>The ministery admins that was founded.</returns>
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(SearchResult<MinistryAdminGettingDto>))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     [HttpGet("GetById")]
     public async Task<IActionResult> GetById(Guid id)
     {
